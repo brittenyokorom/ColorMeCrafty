8d67e12c027e36c94d48c2d682691d4d1b3e5ad9
Britteny Okorom-Achuonye <bokoromachuonye@college.harvard.edu>
2025-01-12 16:03:25 -0500

    base files

---
+++ my-profile-app/src/Profile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Profile() {
+  return (
+    <div>
+      <h2>Profile</h2>
+      {/* Display and edit profile information here */}
+    </div>
+  );
+}
+
+export default Profile;
