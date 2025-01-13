38acdcb4762759ac994d792d4aac8770176f4291
Britteny Okorom-Achuonye <brittenyokorom@gmail.com>
2025-01-12 23:43:04 -0500

    fix deps

--- color-me-crafty/tailwind.config.js
+++ color-me-crafty/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript and TypeScript files in the src directory
+  ],
   theme: {
     extend: {},
   },
