5c5722941a49b10a51fa45692cf48e0294aa3d31
Britteny Okorom-Achuonye <brittenyokorom@gmail.com>
2025-06-17 10:34:07 -0400

    Update App.js
    change homepage url

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/ColorMeCrafty" element={<Home />} />
         <Route path="/create-palette" element={<CreatePalette/>} />
       </Routes>
     </Router>
